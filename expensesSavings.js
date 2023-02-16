b506d3caa2df58234a0852428c7a2b1b56398191
Sayon Usman <sayonusman.ma@gmail.com>
2023-02-16 18:12:55 +0300

    Added validation code.

--- expensesSavings.js
+++ expensesSavings.js
@@ -5,7 +5,24 @@ function calculateExpenses() {
     const rent = document.getElementById('rent');
     const clothes = document.getElementById('clothes');
     const others = document.getElementById('others');
+
+    if (income.value === '' || income.value === '0') {
+        alert('Please enter your monthly income.');
+        return [0, 0];
+    }
+
+    if (food.value === '' || rent.value === '' || clothes.value === '' || others.value === '') {
+        alert('Please enter the amount of money or enter Zero (0) for leave empty.');
+        return [0, 0];
+    }
+
     const totalExpenses = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value) + parseFloat(others.value);
+
+    if (totalExpenses > parseFloat(income.value)) {
+        alert('Your total expenses is greater than your income!!! Please decrease your expenses.')
+        return [0, 0];
+    }
+
     const remainingBalance = parseFloat(income.value) - totalExpenses;
 
     return [totalExpenses, remainingBalance];
@@ -13,8 +30,19 @@ function calculateExpenses() {
 
 function calculateSavings() {
     const savings = document.getElementById('savings');
+    if (savings.value === '') {
+        alert('Please enter the amount of money for savings or enter Zero (0) for leave empty.');
+        return [0, 0];
+    }
+
     const savingsBalance = parseFloat(income.value) * (parseFloat(savings.value) / 100);
     const totalExpensesAndRemainingBalance = calculateExpenses();
+
+    if (savingsBalance > totalExpensesAndRemainingBalance[1]) {
+        alert('Your savings balance is greater than remaining balance!!! Please decrease your expenses.')
+        return [0, 0];
+    }
+
     const remainingBalance = totalExpensesAndRemainingBalance[1] - savingsBalance;
     return [savingsBalance, remainingBalance];
 }
